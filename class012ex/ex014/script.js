927bf6fd36579ee412c270affe44399796f46cec
Angell Belger <angellbelger@hotmail.com>
2022-06-04 22:45:42 -0300

    update

--- class012ex/ex014/script.js
+++ class012ex/ex014/script.js
@@ -5,7 +5,17 @@
     var hour = date.getHours()
     msg.innerHTML = `It is ${hour} hours`
 
-    if (hour >= 7 && hour < 12){
-        image.src = ''
+    if (hour >= 6 && hour < 9){
+        image.src = 'images/photo-morning.png'
+        window.document.body.style.background = '#f0cdb6'
+    }else if (hour >= 9 && hour < 17){
+        image.src = 'images/photo-afternoon.png'
+        window.document.body.style.background = '#2d90bf'
+    }else if ( hour >= 17 && hour < 20){
+        image.src = 'images/photo-evening.png'
+        window.document.body.style.background = '#a64918'
+    }else {
+        image.src = 'images/photo-night.png'
+        window.document.body.style.background = '#0d292a'
     }
  }
