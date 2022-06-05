e94e43ace814128883c53cc21ae732e652dc848d
Angell Belger <angellbelger@hotmail.com>
2022-06-04 22:04:52 -0300

    up

--- class012ex/ex014/script.js
+++ class012ex/ex014/script.js
@@ -1,1 +1,11 @@
-window.alert("Hello, world.")
+ function loading(){
+    var msg = window.document.querySelector('div#msg')
+    var image = window.document.querySelector('img#thumb')
+    var date = new Date()
+    var hour = date.getHours()
+    msg.innerHTML = `It is ${hour} hours`
+
+    if (hour >= 7 && hour < 12){
+        image.src = ''
+    }
+ }
