b05ea437c0898e12715dd1b9b573d3f0f074bcf2
Angell Belger <angellbelger@hotmail.com>
2022-07-23 16:24:09 -0300

    up

--- class014ex/ex016/script.js
+++ class014ex/ex016/script.js
@@ -1,9 +1,12 @@
 var start = window.document.querySelector('input#start')
 var final = window.document.querySelector('input#final')
 var pass = window.document.querySelector('input#pass')
+var result = window.document.querySelector('div#result')
 
 function counting(){
-    for (start; final; pass){
-        document.write('hi')
+    var x = start
+    for (start; final; start + pass){
+        result.innerHTML = `${x}`
+        x = start + pass
     }
 }
