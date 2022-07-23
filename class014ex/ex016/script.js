545e8e6e5b30cf0515710d52005207e9441e1d74
Angell Belger <angellbelger@hotmail.com>
2022-07-23 16:14:34 -0300

    up

---
+++ class014ex/ex016/script.js
@@ -0,0 +1,9 @@
+var start = window.document.querySelector('input#start')
+var final = window.document.querySelector('input#final')
+var pass = window.document.querySelector('input#pass')
+
+function counting(){
+    for (start; final; pass){
+        document.write('hi')
+    }
+}
