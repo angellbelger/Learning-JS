cc059535f3ef9596ce8dfa6f50fe25fe2b08dc81
Angell Belger <angellbelger@hotmail.com>
2022-07-24 17:44:12 -0300

    up

--- class014ex/ex016/script.js
+++ class014ex/ex016/script.js
@@ -1,12 +1,22 @@
-var start = window.document.querySelector('input#start')
-var final = window.document.querySelector('input#final')
-var pass = window.document.querySelector('input#pass')
-var result = window.document.querySelector('div#result')
 
 function counting(){
-    var x = start
-    for (start; start <= final; start + pass){
-        result.innerHTML = `${x}`
-        x = start + pass
+    var txtStart = window.document.querySelector('input#txtStart')
+    var txtFinal = window.document.querySelector('input#txtFinal')
+    var txtPass = window.document.querySelector('input#txtPass')
+    var result = window.document.querySelector('div#result')
+
+    if (txtStart.value.length == 0 || txtFinal.value.length == 0 || txtPass.value.length == 0){
+        window.alert('Please, type a correct value');
+
+    } else {
+        result.innerHTML = 'Counting: '
+        s = Number(txtStart.value)
+        f = Number(txtFinal.value)
+        p = Number(txtPass.value)
+
+        for (var c = s; c <= f; c += p){
+            result.innerHTML += `${c} \u{2728}`
+        }
+        result.innerHTML += `\u{1F51A}`
     }
 }
