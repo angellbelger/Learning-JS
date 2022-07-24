29957f91f12851a497b2ef72f7f2cff31a9025d4
Angell Belger <angellbelger@hotmail.com>
2022-07-24 17:57:45 -0300

    Update script.js

--- class014ex/ex016/script.js
+++ class014ex/ex016/script.js
@@ -7,6 +7,7 @@ function counting(){
 
     if (txtStart.value.length == 0 || txtFinal.value.length == 0 || txtPass.value.length == 0){
         window.alert('Please, type a correct value');
+        result.innerHTML = 'Impossible'
 
     } else {
         result.innerHTML = 'Counting: '
@@ -14,9 +15,22 @@ function counting(){
         f = Number(txtFinal.value)
         p = Number(txtPass.value)
 
-        for (var c = s; c <= f; c += p){
-            result.innerHTML += `${c} \u{2728}`
+        if (p <= 0){
+            result.innerHTML = 'Invalid pass.'
         }
-        result.innerHTML += `\u{1F51A}`
+
+        if (s <= f){
+            for (var c = s; c <= f; c += p){
+                result.innerHTML += `${c} \u{2728}`
+            }
+            result.innerHTML += `\u{1F51A}`
+
+        } else {
+            for (var c = s; c >= f; c -= p){
+                result.innerHTML += `${c} \u{2728}`
+            }
+            result.innerHTML += `\u{1F51A}`
+
+        }
     }
 }
