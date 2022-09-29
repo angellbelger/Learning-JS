18ab6faacf2bb579499ed2b5fe0e6770219d7241
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-09-29 16:33:36 -0300

    update

--- class011/ex008.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello, world.')
-
-var vel = 69
-
-if (vel > 70){
-    console.log('Be careful!')
-}else {
-    console.log('Congratulations! You were fined.')
-}
--- class014/ambiente.js
+++
@@ -1,8 +0,0 @@
-
-console.log('Start');
-
-for (var c = 1; c <= 4; c++){
-    console.log(`${c} - Hello, world.`);
-}
-
-console.log('Game Over', c);
