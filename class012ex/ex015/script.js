11b1731d25f32141bdc9b6f3e31f15546c419e4d
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-01 17:01:20 -0300

    up

--- class012ex/ex015/script.js
+++ class012ex/ex015/script.js
@@ -2,7 +2,7 @@
 function verify(){
     var date = new Date()
     var currentYear = date.getFullYear()
-
     window.document.querySelector('input#yearHim')
-
+    window.document.querySelector('div#result')
+    
 }
