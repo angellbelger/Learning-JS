46c293b5463ee47190e36ed86fd1791591363dac
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-01 15:36:12 -0300

    Update script.js

--- class012ex/ex014/script.js
+++ class012ex/ex014/script.js
@@ -6,18 +6,18 @@ hour = date.getHours()
 
 function dinamic() {
     msg.innerHTML = `The time is ${hour}h.`
-}
 
-if (hour >= 0 && hour < 6){
+    if (hour >= 0 && hour < 6){
     image.src = 'images/photo-night.png'
     window.document.body.style.background = '#0B292B'
-} else if (hour >= 6 && hour < 12){
+    } else if (hour >= 6 && hour < 12){
     image.src = 'images/photo-morning.png'
     window.document.body.style.background = '#EDC4A4'
-} else if (hour >= 12 && hour < 18){
+    } else if (hour >= 12 && hour < 18){
     image.src = 'images/photo-afternoon.png'
     window.document.body.style.background = '#3D9BC3'
-} else if (hour >= 18 && hour <= 23){
+    } else if (hour >= 18 && hour <= 23){
     image.src = 'images/photo-evening.png'
     window.document.body.background = '#D26901'
+    }
 }
