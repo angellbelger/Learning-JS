0ab804cad3072a3da5a3378e55611e1f9a16d8de
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-01 17:20:33 -0300

    up

--- class012ex/ex015/script.js
+++ class012ex/ex015/script.js
@@ -5,8 +5,12 @@ function verify(){
     var yearHim = window.document.querySelector('input#yearHim')
     var result = window.document.querySelector('div#result')
 
+    var gender = ''
     if (yearHim.value.length == 0 || yearHim.value >= currentYear + 1){
         window.alert('Please, type a valid year.')
+    }else {
+        window.document.
+        if ()
     }
     
 }
