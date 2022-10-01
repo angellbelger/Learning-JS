25bf726a3557195344abc0afbf038b85e707efc3
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-01 15:15:47 -0300

    up

--- class012ex/ex014/script.js
+++ class012ex/ex014/script.js
@@ -2,10 +2,14 @@ var msg = window.document.querySelector("div#msgTime")
 var image = window.document.querySelector("img#thumb")
 
 date = new Date()
-hour = date.
+hour = date.getHours()
 
 
 
 function dinamic() {
+    msg.innerHTML = `The time is ${hour}h.`
+}
 
+if (hour >= 6 && hour < 12){
+    image.sou
 }
