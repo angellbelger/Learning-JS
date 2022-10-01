d9ccff4ff53c314b1a4b105c662ea5bbf8ccf776
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-01 17:17:37 -0300

    Update script.js

--- class012ex/ex015/script.js
+++ class012ex/ex015/script.js
@@ -2,7 +2,11 @@
 function verify(){
     var date = new Date()
     var currentYear = date.getFullYear()
-    window.document.querySelector('input#yearHim')
-    window.document.querySelector('div#result')
+    var yearHim = window.document.querySelector('input#yearHim')
+    var result = window.document.querySelector('div#result')
+
+    if (yearHim.value.length == 0 || yearHim.value >= currentYear + 1){
+        window.alert('Please, type a valid year.')
+    }
     
 }
