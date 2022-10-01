49b86e82200e290d8609a34071729578b9c0be04
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-01 15:20:37 -0300

    Update script.js

--- class012ex/ex014/script.js
+++ class012ex/ex014/script.js
@@ -5,11 +5,13 @@ date = new Date()
 hour = date.getHours()
 
 
-
+hour = 8
 function dinamic() {
     msg.innerHTML = `The time is ${hour}h.`
 }
 
 if (hour >= 6 && hour < 12){
-    image.sou
+    image.src = 'images/photo-morning.png'
+    window.document.body.style.background = '#EDC4A4'
+    
 }
