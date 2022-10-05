8693c6702ccdfd6b2dec6b10f22adb2aae96018e
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-05 14:22:13 -0300

    Update script.js

--- class012ex/ex015/script.js
+++ class012ex/ex015/script.js
@@ -2,18 +2,17 @@
 function verify(){
     var date = new Date()
     var currentYear = date.getFullYear()
-    var yearHim = window.document.querySelector('input#yearHim')
+    var himYear = window.document.querySelector('input#yearHim')
     var result = window.document.querySelector('div#result')
 
-    if (yearHim.value.length == 0 || yearHim.value >= currentYear + 1){
+    if (himYear.value.length == 0 || himYear.value >= currentYear + 1){
         window.alert('Please, type a valid year.')
     }else {
         var genderFormu = window.document.getElementsByName('genderRadio')
         var genderTxt = ''
-        var age = currentYear - yearHim.value
+        var age = currentYear - himYear.value
         var image = document.createElement('img')
         image.setAttribute('id', 'photo')
-        
         if (genderFormu[0].checked){
             if (age >= 0 && age < 11)
             image.setAttribute('src', 'photos/photo-baby-boy.png')
