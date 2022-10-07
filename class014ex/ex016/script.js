bf1979bd483d055c26804aa212d34f6eb52d8e54
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-07 15:55:40 -0300

    Update script.js

--- class014ex/ex016/script.js
+++ class014ex/ex016/script.js
@@ -6,8 +6,14 @@ function thePass(){
     var tPass = window.document.querySelector('input#tPass')
 
     if (tStart.value.length == 0 || tEnd.value.length == 0 || tPass.value.length == 0){
-        window.alert('Please, type a value on box.')
+        window.alert('Please, type a value on box.');
         result.innerHTML = 'Please, try again.'
+    }else{
+        var nStart = Number(tStart)
+        var nEnd = Number(tEnd)
+        var nPass = Number(tPass)
+        
+        for ()
     }
 
 
