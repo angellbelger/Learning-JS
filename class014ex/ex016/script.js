56975ba72313f285afb3161ff39551844a9d03f6
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-07 15:42:53 -0300

    up

---
+++ class014ex/ex016/script.js
@@ -0,0 +1,14 @@
+
+function thePass(){
+    var result = window.document.querySelector('div#result')
+    var nStart = window.document.querySelector('input#tStart')
+    var nEnd = window.document.querySelector('input#tEnd')
+    var nPass = window.document.querySelector('input#tPass')
+
+    if (nStart.value.length == 0){
+        window.alert('Please, type a value on Start box.')
+    }
+
+
+
+}
