f184694f691ce380c2cf0380fee222bcbf8c065d
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-19 15:20:08 -0300

    update

---
+++ class014ex/ex017/script.js
@@ -0,0 +1,13 @@
+
+
+function table() {
+    nTxt = window.document.querySelector('input#nTxt')
+    table = window.document.querySelector('select#seltab')
+
+    if (nTxt.value.length == 0){
+        window.alert('Attention! Type a valid number to calculate.')
+
+    }else {
+        numb = Number(nTxt.value)
+    }
+}
