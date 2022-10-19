9fe1d190636bdd067f486835f10f70de9aad58c2
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-19 17:40:40 -0300

    up

--- class015/environment.js
+++ class015/environment.js
@@ -1,3 +1,10 @@
-let numbers = [] 
+let numbers = [5,7,3,2,9] 
 
 console.log(numbers)
+console.log(`Our array is ${numbers}`)
+numbers[5] = 18
+console.log(`Our array is ${numbers}`)
+numbers.push(1)
+console.log(`Our array is ${numbers}`)
+numbers.sort()
+console.log(`The length of its array is ${numbers.length}, and the sorted of array is ${numbers}`)
