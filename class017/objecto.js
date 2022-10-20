6408d0b83a5bb480f7311a43cb706be6e00e8f51
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-20 19:49:59 -0300

    Update objecto.js

--- class017/objecto.js
+++ class017/objecto.js
@@ -1,1 +1,11 @@
-let friend 
+let friend = {name: 'john', 
+gender: 'male', 
+weight: 55,
+bold(w=0){
+    console.log('bolder')
+    this.weight += w
+}
+}
+
+friend.bold(5)
+console.log(`The ${friend.name} weighs ${friend.weight}Kg`)
