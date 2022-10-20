0055ad8c0cbe2c36088c14634c876b4642e21cad
Angell Belger <82967046+angellbelger@users.noreply.github.com>
2022-10-20 19:20:34 -0300

    up

--- final-ex/script.js
+++ final-ex/script.js
@@ -2,6 +2,7 @@
 num = window.document.querySelector('input#number')
 table = window.document.querySelector('select#listnumbers')
 let list = []
+result = window.document.querySelector('div.result')
 
 
 function isNumber(param){
@@ -16,16 +17,12 @@ function isNumber(param){
 
 
 function inList(paramNumber, paramList){
-    for (var c = 0; c < paramList.length; c++){
-        if (Number(paramNumber) == Number(paramList[c])){
-            window.alert('Type a valid number.')
-            return false
-        }else{
-            return true
-        }
+    if (paramList.indexOf(Number(paramNumber)) != -1){
+        return false
+    }else{
+        return true
     }
 }
-
 
 function addNum(){
 
@@ -34,11 +31,41 @@ function addNum(){
 
     }else {
         list.push(Number(num.value))
-
+        result.innerHTML = '<h1>The Result</h1>'
         let item = document.createElement('option')
         item.text = `The value ${num.value} was added.`
         item.value = Number(num.value)
         table.appendChild(item)
 
     }
+    num.value = ''
+    num.focus()
+}
+
+function toFinesh(){
+    if (list.length == 0){
+        window.alert('Please, add values.')
+    }else{
+        var sum = 0
+        result.innerHTML += `<br>Length: ${list.length}<br>The Array: `
+        for (var c = 0; c < list.length; c++){
+            result.innerHTML += `${list[c]} `
+            sum += list[c]
+        }
+        let average = sum / list.length
+        result.innerHTML += `<br>The sum: ${sum}<br>Average: ${average}`
+
+        let lager = list[0]
+        let smaller = list[0]
+        for (let c in list){
+            if (list[c] > lager){
+                lager = list[c]
+            }
+            if (list[c] < smaller){
+                smaller = list[c]
+            }
+        }
+        result.innerHTML += `<br>The lager: ${lager}<br>The smaller: ${smaller}`
+    }
+
 }
